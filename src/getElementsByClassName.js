063b430fe21e62895b92fe8a4397d5046b324382
Kenny Cao <kennyxcao@gmail.com>
2017-08-09 14:45:00 -0700

    Update completed getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,17 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var elements = [];
+
+  var recusiveNodeSearch = function(node) {
+    if (node.classList && node.classList.contains(className))
+      elements.push(node);
+    
+    Array.from(node.childNodes).forEach(recusiveNodeSearch);
+  };
+
+  recusiveNodeSearch(document.body);
+  
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Case: String
+  if (typeof(obj) === 'string')
+    return '"' + obj + '"';
+
+  // Case: Array
+  if (Array.isArray(obj)) {
+    arrStr = obj.map(function(ele) {
+      return stringifyJSON(ele);
+    })
+    return '[' + arrStr.join() + ']';
+  }
+
+  // Case: Object
+  if (obj && typeof(obj) === 'object') {
+    var objStr = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof(obj[key]) === 'function')
+        continue;
+      objStr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + objStr.join() + '}';
+  }
+
+  // Case: Number, Null, Boolean
+  return '' + obj;
 };
